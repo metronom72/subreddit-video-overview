f3d9ebd8f9242ebf26322a7c75b0c0273a25443a
Dorokhovich Michael <mikhail@dorokhovich.ru>
2024-06-21 01:20:44 +0200

    wip: upd js script

--- src/html_generation/templates/v3/script.js
+++ src/html_generation/templates/v3/script.js
@@ -4,35 +4,43 @@ function getDurationFromUrl() {
 }
 
 function joinArrayWithBr(array, node) {
-  // Ensure the node is a valid DOM element
-  if (!(node instanceof HTMLElement)) {
-    throw new Error("The second argument must be a valid DOM element.");
-  }
+    // Ensure the node is a valid DOM element
+    if (!(node instanceof HTMLElement)) {
+        throw new Error("The second argument must be a valid DOM element.");
+    }
 
-  // Join the array elements with <br> tags
-  // Append the resulting string to the specified node
-  node.innerHTML = array.join('<br>');
+    // clear the element
+    node.innerHTML = ""
+
+    // Join the array elements with <br> tags
+    // Append the resulting string to the specified node
+    node.innerHTML = array.join('<br>');
 }
 
 function getChunks(paragraphs, chunkSize = 150) {
     //[paragraph, paragraph, paragraph] [[word, word, ...], [word, word, ...], [word, word, ...]]
     paragraphs = paragraphs.map(paragraph => paragraph.split(/\s+/))
 
-    const chunks = []
+    const chunks = [[]]
+
+    function lastChunkIndex() {
+        return chunks.length - 1
+    }
+
+    function lastParagraphIndex() {
+        return chunks[lastChunkIndex()].length - 1
+    }
+
     paragraphs.forEach((paragraph, pIndex) => {
-        const lastChIndex = chunks.length - 1
-        if (chunks.length === 0) {
-            chunks.push([])
-        }
-        const lastPIndex = chunks[lastChIndex].length - 1
-        if (pIndex === 0) {
-            chunks[chunks.length - 1].push([])
-        }
-        paragraph.map((word, sIndex) => {
-            if (chunks[lastChIndex][lastPIndex].length < chunkSize) {
-                chunks[lastChIndex][lastPIndex].push(word);
+        paragraph.forEach((word, sIndex) => {
+            if (sIndex === 0) {
+                chunks[lastChunkIndex()].push([])
+            }
+
+            if (chunks[lastChunkIndex()].flat().length < chunkSize) {
+                chunks[lastChunkIndex()][lastParagraphIndex()].push(word);
             } else {
-                chunks.push([word])
+                chunks.push([[]])
             }
         })
     })
@@ -41,43 +49,54 @@ function getChunks(paragraphs, chunkSize = 150) {
 }
 
 function animateText(element) {
-    const text = element.innerHTML;
-    const words = text.split(/\s+/);
-    element.innerHTML = '';
-
+    const chunks = getChunks(window.comment)
     const totalDuration = getDurationFromUrl();
-    const totalChunks = Math.ceil(words.length / 5);
-    const chunkDuration = totalDuration / totalChunks;
+    const wordsList = chunks.flat().flat()
+    const sectionDuration = Math.ceil(totalDuration / chunks.length)
+    let sectionDurations = chunks.map((chunk) => chunk.flat().length / wordsList.length)
+    const totalLengthRelative = sectionDurations.reduce((acc, val) => acc + val, 0)
+    sectionDurations = sectionDurations.map((t) => t / totalLengthRelative * totalDuration)
 
-    words.forEach((word, index) => {
-        const span = document.createElement('span');
-        span.innerHTML = word + ' ';
-        span.classList.add('hidden');
-        element.appendChild(span);
+    chunks.forEach((chunk, chIndex) => {
+        const pastDuration = sectionDurations.slice(0, chIndex + 1).reduce((acc, val) => acc + val, 0)
 
-        const chunkIndex = Math.floor(index / 5);
         setTimeout(() => {
-            for (let i = chunkIndex * 5; i < (chunkIndex + 1) * 5 && i < words.length; i++) {
-                element.children[i].classList.add('visible');
-            }
-            if (index === words.length - 1) {
-                setTimeout(() => {
-                    const body = document.querySelector('body');
-                    body.setAttribute('data-rendering-over', true);
-                }, 2000);
+            element.innerHTML = ''
+            chunk = chunk.flat()
+            const totalChunks = Math.ceil(chunk.length / 5);
+            const chunkDuration = sectionDurations[chIndex] / totalChunks;
 
+            chunk.forEach((word, index) => {
+                const span = document.createElement('span');
+                span.innerHTML = word + ' ';
+                span.classList.add('hidden');
+                element.appendChild(span);
+
+                const chunkIndex = Math.floor(index / 5);
                 setTimeout(() => {
-                    const body = document.body;
-                    body.style.cssText = 'opacity: 0;';
-                }, 750);
-            }
-        }, chunkIndex * chunkDuration);
-    });
+                    for (let i = chunkIndex * 5; i < (chunkIndex + 1) * 5 && i < chunk.length; i++) {
+                        element.children[i].classList.add('visible');
+                    }
+
+                    if (index === chunk.length - 1 && chIndex === chunks.length - 1) {
+                        setTimeout(() => {
+                            const body = document.querySelector('body');
+                            body.setAttribute('data-rendering-over', true);
+                        }, 2000);
+
+                        setTimeout(() => {
+                            const body = document.body;
+                            body.style.cssText = 'opacity: 0;';
+                        }, 750);
+                    }
+                }, chunkIndex * chunkDuration);
+            });
+            console.log(chIndex, pastDuration, pastDuration + sectionDurations[chIndex])
+        }, pastDuration - sectionDurations[chIndex])
+    })
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    joinArrayWithBr(window.comment, document.getElementById('comment1'));
-    console.log(getChunks(window.comment), window.comment)
     setTimeout(() => {
         const commentText = document.getElementById('comment1');
         commentText.style.cssText = 'opacity: 1;';
